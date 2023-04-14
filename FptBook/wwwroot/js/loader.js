c529942e66d7d744f28a41641cfd9b8a78f05207
IriS <mhuynk1005@gmail.com>
2023-04-14 18:40:22 +0700

    Add loader function to load the book icon when visiting the landing page

---
+++ FptBook/wwwroot/js/loader.js
@@ -0,0 +1,10 @@
+﻿window.onload = () =>{
+    fadeOut();
+}
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+    setTimeout(loader, 4000);
+}
