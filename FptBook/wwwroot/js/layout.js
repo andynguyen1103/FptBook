744796f21d55f488fbd74cb99a0374c7e0f6d52f
1Ir-is <mhuynk1005@gmail.com>
2023-04-16 12:29:21 +0700

    Add js for blog and review section, redesign for those section

--- FptBook/wwwroot/js/layout.js
+++
@@ -1,9 +0,0 @@
-﻿let loginForm = document.querySelector('.login-form-container');
-
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-}
-
-document.querySelector('#close-login-btn').onclick = () =>{
-    loginForm.classList.remove('active');
-}
--- FptBook/wwwroot/js/site.js
+++ FptBook/wwwroot/js/site.js
@@ -4,3 +4,85 @@
 // Write your JavaScript code.
 
 
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    if(window.scrollY > 80)
+    {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else
+    {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 80)
+    {
+        document.querySelector('.header .header-2').classList.add('active');
+    }
+    else
+    {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+    fadeOut();
+}
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('active');
+}
+
+function fadeOut(){
+    setTimeout(loader, 4000);
+}
+
+const swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+});
+
+const blog_swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+});
