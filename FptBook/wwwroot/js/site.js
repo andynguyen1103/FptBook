25c3371d615ca2eb3b28171d0c88838b00a1f0d4
1Ir-is <mhuynk1005@gmail.com>
2023-04-18 19:50:24 +0700

    Add background image for all form

--- FptBook/wwwroot/js/site.js
+++ FptBook/wwwroot/js/site.js
@@ -11,26 +11,9 @@ document.querySelector('#search-btn').onclick = () =>{
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
-    if(window.scrollY > 80)
-    {
-        document.querySelector('.header .header-2').classList.add('active');
-    }
-    else
-    {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
 }
 
 window.onload = () =>{
-    if(window.scrollY > 80)
-    {
-        document.querySelector('.header .header-2').classList.add('active');
-    }
-    else
-    {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
-
     fadeOut();
 }
 
