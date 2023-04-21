371f3367c5eb2513f6e9aee670fd5f6bb334c1f0
andynguyen1103 <andynguyenvn1103@gmail.com>
2023-04-21 11:14:31 +0700

    Finish adding store owner function for admin

--- FptBook/Areas/Admin/Controllers/User/UserController.cs
+++ FptBook/Areas/Admin/Controllers/User/UserController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using FptBook.Areas.Identity.Data;
 using FptBook.Models;
 using Microsoft.AspNetCore.Identity;
@@ -27,6 +28,67 @@ namespace FptBook.Areas.Admin.Controllers.User
               return _context.Users != null ? 
                           View(await _context.Users.ToListAsync()) :
                           Problem("Entity set 'FptBookIdentityDbContext.Users'  is null.");
+        }
+        public class UserInput
+        {
+            [EmailAddress]
+            [Required(ErrorMessage = "Missing email address")]
+            public string Email { get; set; }
+            
+            [Required(ErrorMessage = "You must enter your first name!")]
+            [Display(Name = "First Name")]
+            [StringLength(100,ErrorMessage = "First name too long!")]
+            public string FirstName { get; set; }
+            
+            [Required(ErrorMessage = "You must enter your last name")]
+            [Display(Name = "Last Name")]
+            [StringLength(100,ErrorMessage = "Last name too long!")]
+            public string LastName { get; set; }
+            
+            [Required(ErrorMessage = "You must enter a phone number to register!")]
+            [Display(Name = "Phone Number")]
+            [Phone] 
+            public string PhoneNumber { get; set; }
+        }
+
+        [HttpGet("CreateManager")]
+        public async Task<IActionResult> CreateManager()
+        {
+            var userInput = new UserInput();
+            return View(userInput);
+        }
+        
+        
+        [HttpPost("CreateManager")]
+        public async Task<IActionResult> CreateManager(UserInput userInput)
+        {
+            var user = new FptBookUser()
+            {
+                Email = userInput.Email,
+                UserName = userInput.Email,
+                FirstName = userInput.FirstName,
+                LastName = userInput.LastName,
+                EmailConfirmed = true
+            };
+            // Console.WriteLine("user name:"+user.Email);
+            //add all the information
+            //Im lazy so the password is the phone number
+            
+            var createResult = await _userManager.CreateAsync(user, "manager12345");
+            if (createResult.Succeeded)
+            {
+                Console.WriteLine("Create User Success");
+                await _userManager.SetPhoneNumberAsync(user, userInput.PhoneNumber);
+                await _userManager.AddToRoleAsync(user, RoleNames.StoreManager);
+            }
+            else
+            {
+                Console.WriteLine(createResult.Errors);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
 
         // GET: User/Details/5
