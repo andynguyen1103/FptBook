4acd2394c6495ea736ef956381a1fba099cff594
IriS <mhuynk1005@gmail.com>
2023-04-24 16:21:52 +0700

    Redesign header for store manager and admin page

--- FptBook/Models/CategoryRequest.cs
+++ FptBook/Models/CategoryRequest.cs
@@ -13,20 +13,25 @@ public class CategoryRequest
     
     [StringLength(50,ErrorMessage = "Category name too long!")]
     [Required]
-    [Display(Name = "Category")]
+    [Display(Name = "Category Name ")]
     public string? Name { get; set; }
 
     [DataType (DataType.Date)]
+    [Display(Name = "Day Created")]
     public DateTime CreatedAt { get; set; }
     
+    [Display(Name = "Approved Status")]
     public bool? IsApproved { get; set; }
     
+    [Display(Name = "Day Approved")]
     public DateTime? ApprovedAt { get; set; }
     
     [Required]
     public string? UserID { set; get; }
     
     [ForeignKey("UserID")]
+        
+    [Display(Name = "User Email")]
     public FptBookUser? User { get; set; }
 
     public CategoryRequest()
