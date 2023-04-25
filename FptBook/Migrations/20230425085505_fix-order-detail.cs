98e91b2383d2e562bb5c23e000fe725331c66e52
andynguyen1103 <andynguyenvn1103@gmail.com>
2023-04-25 17:14:10 +0700

    Complete ordering feature

---
+++ FptBook/Migrations/20230425085505_fix-order-detail.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FptBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixorderdetail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Books_ProductID",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderID",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_ProductID",
+                table: "OrderDetails");
+
+            migrationBuilder.DropColumn(
+                name: "ProductID",
+                table: "OrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderID",
+                table: "OrderDetails",
+                newName: "OrderId");
+
+            migrationBuilder.RenameColumn(
+                name: "BookID",
+                table: "OrderDetails",
+                newName: "BookId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetails_OrderID",
+                table: "OrderDetails",
+                newName: "IX_OrderDetails_OrderId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookId",
+                table: "OrderDetails",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_BookId",
+                table: "OrderDetails",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Books_BookId",
+                table: "OrderDetails",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Books_BookId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderDetails_BookId",
+                table: "OrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "OrderDetails",
+                newName: "OrderID");
+
+            migrationBuilder.RenameColumn(
+                name: "BookId",
+                table: "OrderDetails",
+                newName: "BookID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                newName: "IX_OrderDetails_OrderID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BookID",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductID",
+                table: "OrderDetails",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductID",
+                table: "OrderDetails",
+                column: "ProductID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Books_ProductID",
+                table: "OrderDetails",
+                column: "ProductID",
+                principalTable: "Books",
+                principalColumn: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderID",
+                table: "OrderDetails",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
